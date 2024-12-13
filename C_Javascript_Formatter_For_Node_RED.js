45fe53275231afd37f5b21dd7cfa4bcfdae8e2e0
TCT-IoT <guillaume.morette@tct.fr>
2024-12-13 15:40:09 +0100

    Update C_Javascript_Formatter_For_Node_RED.js

--- C_Javascript_Formatter_For_Node_RED.js
+++ C_Javascript_Formatter_For_Node_RED.js
@@ -1,19 +1,18 @@
 var hexData = msg.payload;
-var devAddr = msg.rawdata.devaddr;
+var devAddr = msg.rawdata.deveui;
 
-// We want to retrieve the last 8 characters of the devAddr
-devAddr = devAddr.substring(devAddr.length - 8);
+// We want to retrieve the last 5 characters of the deveui
+devAddr = devAddr.substring(devAddr.length - 5);
 
 var hexToDecimal = hex => parseInt(hex, 16);
-
 hexData = hexData.toUpperCase();
 
-// Possible types of measurements
+// Possible measurement types
 // These values come from the sensor's LoRa documentation.
 var typeReferences = {
-    '08': 'Temperature:' + devAddr,
-    '0A': 'Voltage:' + devAddr,
-    '0B': 'Current:' + devAddr
+    '08': devAddr + ':temperature',
+    '0A': devAddr + ':voltage',
+    '0B': devAddr + ':current'
 };
 
 // Corresponding values to get °C, V, and A
@@ -27,12 +26,11 @@ var dividerReferences = {
 
 // Frame parameters
 // a0 = Measurement frame, no history, 1 sample
-
 var currentDate = new Date();
 var loraPacketType = hexData[0] + hexData[1];
 var nbSamples = hexToDecimal(hexData[1]) + 1;
-// If we have more than one sample, we retrieve the transmission frequency
 
+// If we have more than one sample, we retrieve the transmission frequency
 var emissionFrequency = 0;
 if (nbSamples > 1) {
     emissionFrequency = hexToDecimal(hexData[2] + hexData[3] + hexData[4] + hexData[5]) / nbSamples;
@@ -40,23 +38,21 @@ if (nbSamples > 1) {
 
 // Formatted data
 var decryptedDatas = {};
-
 var dataIndex = 2 + (nbSamples > 1 ? 4 : 0);
 var readingDataType = 0;
+
 // Each data point is coded on x bits, 2 bits for the data type and 4*nbSamples for the value
 while (dataIndex < hexData.length) {
     // Retrieve the data type
     var dataTypeValue = hexData[dataIndex] + hexData[dataIndex + 1];
-    // Then get the human-readable correspondence
+    // Then the human-readable correspondence
     var dataType = typeReferences[dataTypeValue];
     dataIndex += 2;
     decryptedDatas[readingDataType] = [];
-
     var stopReadingValues = dataIndex + 4 * nbSamples;
 
     while (dataIndex < stopReadingValues) {
         var currentValues = {};
-
         // Next, retrieve the measurement value
         var data = "";
         var stopReadingDigit = dataIndex + 4;
@@ -67,14 +63,12 @@ while (dataIndex < hexData.length) {
         // Finally, convert the measurement to decimal and divide it to work with the desired units
         var decimalData = parseInt(data, 16);
         currentValues[dataType] = decimalData / dividerReferences[dataTypeValue];
-
         decryptedDatas[readingDataType].push(currentValues);
     }
     readingDataType += 1;
 }
 
 var combinedData = {};
-
 var keys = Object.keys(decryptedDatas);
 
 for (var k = 0; k < keys.length; k++) {
@@ -93,7 +87,6 @@ var messages = [];
 for (var i = 0; i < nbSamples; i++) {
     var sampleDate = new Date();
     sampleDate.setMinutes(currentDate.getMinutes() - emissionFrequency * (nbSamples - i - 1));
-
     var sample = {};
     sample.payload = {
         'd': {},
@@ -101,11 +94,9 @@ for (var i = 0; i < nbSamples; i++) {
     };
     sample.payload['d'][devAddr] = {};
     sample.payload['d'][devAddr]['Val'] = {};
-
     sample.payload['d'][devAddr]['Val'] = combinedData[i];
+    sample['topic'] = msg.topic;
     messages.push(sample);
 }
 
-msg.payload = messages;
-
-return msg;
+return [messages];
